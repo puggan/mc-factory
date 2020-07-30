c5a1343d6518447c1f7b50ff8a2b584371357c80
Thorbjörn Sundragon <puggan@spiro.se>
2020-07-30 20:35:49 +0200

    Fabric: DummyContainer

--- src/main/java/se/puggan/factory/container/DummyContainer.java
+++ src/main/java/se/puggan/factory/container/DummyContainer.java
@@ -1,21 +1,21 @@
 package se.puggan.factory.container;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
 
-public class DummyContainer extends Container {
+public class DummyContainer extends ScreenHandler {
     protected DummyContainer() {
-        super(ContainerType.CRAFTING, 0);
+        super(ScreenHandlerType.CRAFTING, 0);
     }
 
     @Override
-    public boolean canInteractWith(PlayerEntity playerIn) {
+    public boolean canUse(PlayerEntity playerIn) {
         return false;
     }
 
     @Override
-    public void onCraftMatrixChanged(IInventory inventoryIn) {
+    public void onContentChanged(Inventory inventoryIn) {
     }
 }
